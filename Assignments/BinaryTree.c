e54ad729212898ac678c29059b236e5240be09ba
Fatih ÇALIŞIR <27806840+mithrilfire@users.noreply.github.com>
2020-12-23 20:48:28 +0300

    Added Breadth-First Search to Binary Tree Assignment

--- Assignments/BinaryTree.c
+++ Assignments/BinaryTree.c
@@ -1,136 +1,224 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node
+struct binary_tree
 {
     int value;
-    struct node *left_child;
-    struct node *right_child;
+    struct binary_tree *left_child;
+    struct binary_tree *right_child;
 };
 
-/*
-void insert_node_left(struct node *node, int value)
+struct queue
+{
+    struct binary_tree *data;
+    struct queue *next;
+};
+
+struct binary_tree *create_node(int value)
 {
 
-    struct node *new_node = (struct node *) malloc(sizeof(struct node));
+    struct binary_tree *new_node = (struct binary_tree *) malloc(sizeof(struct binary_tree));
+
     new_node->value = value;
+    new_node->left_child = NULL;
+    new_node->right_child = NULL;
+
+    return new_node;
+
+}
+
+void insert_node_left(struct binary_tree *node, struct binary_tree *new_node)
+{
 
     if (node->left_child)
     {
+
         new_node->left_child = node->left_child;
+        node->left_child = new_node;
+
     }
     else
     {
+
         node->left_child = new_node;
+
     }
 
 }
 
-void insert_node_right(struct node *node, int value)
+void insert_node_right(struct binary_tree *node, struct binary_tree *new_node)
 {
-
-    struct node *new_node = (struct node *) malloc(sizeof(struct node));
-    node->right_child->value = value;
 
     if (node->right_child)
     {
+
         new_node->right_child = node->right_child;
+        node->right_child = new_node;
+
     }
     else
     {
+
         node->right_child = new_node;
+
     }
 
 }
-*/
 
-void print_pre_order_dfs(struct node *head_node)
+void print_pre_order(struct binary_tree *node)
 {
 
-    printf("%d\n", head_node->value);
+    printf("\t%d\n", node->value);
 
-    if (head_node->left_child)
+    if (node->left_child)
     {
-        print_pre_order_dfs(head_node->left_child);
+
+        print_pre_order(node->left_child);
+
     }
 
-    if (head_node->right_child)
+    if (node->right_child)
     {
-        print_pre_order_dfs(head_node->right_child);
+
+        print_pre_order(node->right_child);
+        
     }
 
 }
 
-void print_in_order_dfs(struct node *head_node)
+void print_in_order(struct binary_tree *node)
 {
 
-    if (head_node->left_child)
+    if (node->left_child)
     {
-        print_in_order_dfs(head_node->left_child);
+
+        print_in_order(node->left_child);
+
     }
 
-    printf("%d\n", head_node->value);
+    printf("\t%d\n", node->value);
 
-    if (head_node->right_child)
+    if (node->right_child)
     {
-        print_in_order_dfs(head_node->right_child);
+
+        print_in_order(node->right_child);
+
     }
 
 }
 
-void print_post_order_dfs(struct node *head_node)
+void print_post_order(struct binary_tree *node)
 {
 
-    if (head_node->left_child)
+    if (node->left_child)
     {
-        print_in_order_dfs(head_node->left_child);
+
+        print_post_order(node->left_child);
+
     }
 
-    if (head_node->right_child)
+    if (node->right_child)
     {
-        print_in_order_dfs(head_node->right_child);
+
+        print_post_order(node->right_child);
+
     }
 
-    printf("%d\n", head_node->value);
+    printf("\t%d\n", node->value);
 
 }
-/*
-void print_bfs()
+
+void enqueue(struct queue **queue_head, struct binary_tree *data)
 {
 
+    struct queue *new_e = (struct queue *) malloc(sizeof(struct queue));
+    struct queue *iter;
+
+    new_e->data = data;
+    new_e->next = NULL;
+
+    if (!(*queue_head))
+    {
+        *queue_head = new_e;
+        return;
+    }
+    
+    iter = *queue_head;
+
+    while (iter->next)
+    {
+        iter = iter->next;
+    }
+    
+    iter->next = new_e;
+
 }
-*/
-int main() {
 
-    struct node * binary_list = NULL;
+struct binary_tree *dequeue(struct queue **queue_head)
+{
 
-    struct node *new_node = (struct node *) malloc(sizeof(struct node));
-    new_node->right_child = NULL;
-    new_node->left_child = NULL;
-    new_node->value = 2;
-    struct node *new_node2 = (struct node *) malloc(sizeof(struct node));
-    new_node2->right_child = NULL;
-    new_node2->left_child = NULL;
-    new_node2->value = 3;
-    struct node *new_node3 = (struct node *) malloc(sizeof(struct node));
-    new_node3->right_child = NULL;
-    new_node3->left_child = NULL;
-    new_node3->value = 1;
-    struct node *new_node4 = (struct node *) malloc(sizeof(struct node));
-    new_node4->right_child = NULL;
-    new_node4->left_child = NULL;
-    new_node4->value = 4;
+    struct binary_tree *result;
 
-    new_node2->right_child = new_node4;
-    new_node->right_child = new_node2;
-    new_node->left_child = new_node3;
+    if (!(*queue_head))
+    {
+        return NULL;
+    }
+    
+    result = (*queue_head)->data;
+    
+    if ((*queue_head)->next)
+    {
+        *queue_head = (*queue_head)->next;
+    }
+    else
+    {
+        *queue_head = NULL;
+    }
 
-    binary_list = new_node;
+    return result;
+}
 
-    print_in_order_dfs(binary_list);
-    print_pre_order_dfs(binary_list);
-    print_post_order_dfs(binary_list);
-    printf("end");
+void print_bfs(struct binary_tree *tree_head)
+{
 
+    struct queue *new_queue = NULL;
+    struct binary_tree *node = NULL;
+
+    node = tree_head;
+
+    while (node != NULL)
+    {
+
+        printf("\t%d\n", node->value);
+
+        enqueue(&new_queue, node->left_child);
+        enqueue(&new_queue, node->right_child);
+
+        node = dequeue(&new_queue);
+
+    }
+
+}
+
+int main()
+{
+
+    struct binary_tree *binary_list = NULL;
+    binary_list = create_node(1);
+    insert_node_left(binary_list, create_node(2));
+    insert_node_right(binary_list, create_node(3));
+    insert_node_left(binary_list->left_child, create_node(4));
+    insert_node_right(binary_list->left_child, create_node(5));
+
+    printf("-------<->-------\n\n");
+    print_pre_order(binary_list);
+    printf("\n-------<O>-------\n\n");
+    print_in_order(binary_list);
+    printf("\n-------<+>-------\n\n");
+    print_post_order(binary_list);
+    printf("\n-------<V>-------\n\n");
+    print_bfs(binary_list);
+    
     return 0;
+
 }
